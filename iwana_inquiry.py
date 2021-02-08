202f35593fa6abeec07a86fcf260c05b102e18ba
maru44 <takumaru22244@gmail.com>
2021-02-08 19:42:48 +0900

    send mail to user

--- iwana_inquiry.py
+++ iwana_inquiry.py
@@ -1,14 +1,47 @@
 import json
 import boto3
 
+# for date and time
 import re, datetime
-
+# for generate id
 import random, string
 
+"""    DynamoDB    """
 from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table("inqury_table")
+
+"""    Email    """
+import smtplib
+from email.mime.text import MIMEText
+
+from_email = "<Email>"
+password = '<Password>'
+email_port = 587
+
+subject_user = "Iwana: お問い合わせにつきまして"
+subject_owner = "Iwana: お問い合わせの連絡"
+
+message_user = """
+<h3>{user_} 様</h3>
+
+お問い合わせありがとうございます。<br/>
+こちらのメールは自動送信メールです。<br/>
+<br/>
+以下の内容で承りました。<br/>
+<br/>
+==============================<br/>
+<h4>項目</h4>{category_}
+
+<h4>内容</h4>{content_}<br/>
+<br/>
+==============================<br/>
+<br/>
+Iwana公式<br/>
+{from_}
+"""
+
 
 # generate id
 def gen_id():
@@ -25,6 +58,20 @@ def gen_datetime():
         int(re.sub(r'\.[\d]*', '', str_time.replace(":", "")))
     ]
     return lst
+    
+# send email
+def sending_user(name, mail, content, category):
+    message = message_user.format(from_=from_email, user_=name, content_=content, category_=category)
+    msg = MIMEText(message, "html")
+    msg["Subject"] = subject_user
+    msg["To"] = mail
+    msg["From"] = from_email
+    
+    server = smtplib.SMTP("smtp.gmail.com", email_port)
+    server.starttls()
+    server.login(from_email, password)
+    server.send_message(msg)
+    server.quit()
     
 def operation_scan():
     scanData = table.scan()# get all
@@ -49,7 +96,7 @@ def operation_put(id, name, mail, content, category):
     if putResponse['ResponseMetadata']['HTTPStatusCode'] != 200:
         print(putResponse)
     else:
-        print('Putted')
+        sending_user(name, mail, content, category)
     return putResponse
 
 def lambda_handler(event, context):
